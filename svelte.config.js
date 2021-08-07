35a3f7383dd57774e2c32654e2af275504fce528
Lucas L. Treffenstädt <lucas@treffenstaedt.de>
2021-08-07 15:53:07 +0200

    fix: specify relative path in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,7 @@ const sveltePreprocess = require('svelte-preprocess');
 
 module.exports = {
     preprocess: sveltePreprocess(),
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/dog-memory/'
+        : '/'
 };
